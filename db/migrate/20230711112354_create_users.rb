ebbcb4aae900d9b61dd14c2a7269711607f49e14
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-11 13:01:33 +0100

    Add missing colums

--- db/migrate/20230711112354_create_users.rb
+++ db/migrate/20230711112354_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.integer :author_id, null: false
       t.string :name
       t.string :photo
       t.text :bio
--- db/migrate/20230711112604_create_posts.rb
+++ db/migrate/20230711112604_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.integer :author_id
+      t.string :title
       t.text :text
       t.datetime :created_at
       t.datetime :updated_at
