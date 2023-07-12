6645751f8a36a2df817b6a45192cae89f343637d
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-12 21:47:06 +0100

    Apply some modifications to resolve the errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :posts
 
   validates :author_id, presence: true
   validates :post_id, presence: true
   validates :text, presence: true
   #  A method that updates the comments counter for a post.
   def update_comments_counter
-    post.increment!(:comments_counter)
+    posts.increment!(:comments_counter)
   end
 end
