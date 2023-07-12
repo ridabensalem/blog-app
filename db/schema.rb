bf7530d5fd16b230d1b4b73ee62a6501e64ff7d1
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-12 16:14:53 +0100

    solved migrations errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_140600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comment", id: :serial, force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "post_id", null: false
+    t.text "text", null: false
+    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+  end
+
+  create_table "likes", id: :serial, force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+  end
+
+  create_table "post", id: :serial, force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.string "title", limit: 255, null: false
+    t.text "text", null: false
+    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+  end
+
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
+  end
+
+  add_foreign_key "comment", "post", name: "comment_post_id_fkey"
+  add_foreign_key "comment", "users", column: "author_id", name: "comment_author_id_fkey"
+  add_foreign_key "likes", "post", name: "likes_post_id_fkey"
+  add_foreign_key "likes", "users", column: "author_id", name: "likes_author_id_fkey"
+  add_foreign_key "post", "users", column: "author_id", name: "post_author_id_fkey"
 end
