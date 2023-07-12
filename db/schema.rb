bb3941dfa646b493950a24060ee17dcadac05d14
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-12 21:47:36 +0100

    Applied reviewer chnges

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_140600) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_190701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comment", id: :serial, force: :cascade do |t|
-    t.integer "author_id", null: false
-    t.integer "post_id", null: false
-    t.text "text", null: false
-    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "likes", id: :serial, force: :cascade do |t|
-    t.integer "author_id", null: false
-    t.integer "post_id", null: false
-    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+  create_table "likes", force: :cascade do |t|
+    t.bigint "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
   end
 
-  create_table "post", id: :serial, force: :cascade do |t|
-    t.integer "author_id", null: false
-    t.string "title", limit: 255, null: false
-    t.text "text", null: false
-    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+  create_table "posts", force: :cascade do |t|
+    t.bigint "author_id"
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
     t.text "bio"
     t.integer "posts_counter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comment", "post", name: "comment_post_id_fkey"
-  add_foreign_key "comment", "users", column: "author_id", name: "comment_author_id_fkey"
-  add_foreign_key "likes", "post", name: "likes_post_id_fkey"
-  add_foreign_key "likes", "users", column: "author_id", name: "likes_author_id_fkey"
-  add_foreign_key "post", "users", column: "author_id", name: "post_author_id_fkey"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
