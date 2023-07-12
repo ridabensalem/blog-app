9b7ba9df921e404ef25f95d6a00cc60ff14b3db4
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-12 12:45:25 +0100

    Apply requirements

--- db/migrate/20230711111829_create_comments.rb
+++ db/migrate/20230711111829_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.integer :author_id, null: false
-      t,integer :post_id, null: false
+      t.integer :post_id, null: false
       t.text :text, null: false
       t.references :user, foreign_key: { on_delete: :cascade }
       t.references :post, foreign_key: { on_delete: :cascade }
