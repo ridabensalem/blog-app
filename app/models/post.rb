38627adaed32c548bf953787b8c230039cb835a8
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-14 16:43:36 +0100

    Add more methodes of validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,17 @@ class Post < ApplicationRecord
   has_many :likes
 
   validates :author_id, presence: true
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # A method that updates the posts counter for a user.
   def update_posts_counter
     update(posts_counter: posts.count)
   end
 
-  # A method that returns the 5 most recent posts for a given post.
+  # A method that returns the 5 most recent comments for a given post.
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
