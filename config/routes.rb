b5742eb53d1f0679be6a823f38a2baa5af975103
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-18 11:45:26 +0100

    fix the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # config/routes.rb
 Rails.application.routes.draw do
-  # List all posts for a given user
-  get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:new, :create]
+    end
+  end
 
-  # Show a specific post
-  get '/posts/:id', to: 'posts#show', as: :post
-
-  # Create a new comment for a specific post
-  get '/posts/:post_id/comments/new', to: 'comments#new', as: :new_comment
-  post '/posts/:post_id/comments', to: 'comments#create', as: :comments
+  resources :posts, only: [:show] do
+    resources :likes, only: [:create, :destroy]
+  end
 end
