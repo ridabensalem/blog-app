094260b146824b77cb4aedc82772feba533b4b75
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-18 10:22:04 +0100

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # List all posts for a given user
+  get '/users/:user_id/posts', to: 'posts#index', as: :user_posts
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Show a specific post
+  get '/posts/:id', to: 'posts#show', as: :post
+
+  # Create a new comment for a specific post
+  get '/posts/:post_id/comments/new', to: 'comments#new', as: :new_comment
+  post '/posts/:post_id/comments', to: 'comments#create', as: :comments
 end
