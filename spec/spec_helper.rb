a8beb39cfa111e6431a9ed50f92196e9f77cc9ae
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-25 11:57:53 +0100

    Edit  the spec_helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -89,5 +89,4 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
-  # make the tests silent
 end
