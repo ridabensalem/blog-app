a66ce51464d787a277630fd6110b22653f251f61
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-27 12:58:48 +0100

    Install tailwind css

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  root "users#index"
+  
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show] do
-      resources :comments, only: [:create, :destroy]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:create, :new, :destroy]
+      resources :likes, only: [:create]
     end
   end
 
-  resources :posts, only: [:index, :show] do
-    resources :likes, only: [:create, :destroy]
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
   end
 end
