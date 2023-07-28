780931d4885747be69caa9d5081ca34d2fe5b334
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-28 18:20:32 +0100

    fix the routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,5 @@ Rails.application.routes.draw do
     end
   end
 
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:index, :show] do
-        resources :posts, only: [:index] do
-          resources :comments, only: [:index, :create]
-        end
-      end
-    end
-  end
+ 
 end
