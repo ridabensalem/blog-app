2c664f09c366bafb518b3242ba0048791295631a
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-28 18:20:12 +0100

    Fix controllers issues

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
-# app/controllers/posts_controller.rb
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:comments, :likes)
+    @post = Post.new
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
 end
