9fc6b5775be49f1f3736d2ca08b6c10a6b832b74
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-07-31 10:33:49 +0100

    Add extra gems

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -3,29 +3,29 @@ require 'rails_helper'
 
 RSpec.describe 'User show page', type: :feature do
   before do
-   scenario 'I can see the user profile picture.' do 
-        visit user_path
-        expect(page).to have_css("img[src='#{user.photo}']") if user.photo.present?
+    scenario 'I can see the user profile picture.' do
+      visit user_path
+      expect(page).to have_css("img[src='#{user.photo}']") if user.photo.present?
     end
     scenario 'I can see the user name.' do
-        visit user_path
-        expect(page).to have_content(user.name)
+      visit user_path
+      expect(page).to have_content(user.name)
     end
     scenario 'I can see three posts.' do
-        visit user_path
-        expect(page).to have_content(user.posts)
+      visit user_path
+      expect(page).to have_content(user.posts)
     end
     scenario 'I can see the number of posts the user has written.' do
-        visit user_path
-        expect(page).to have_content("Number of posts: #{user.posts_counter}")
+      visit user_path
+      expect(page).to have_content("Number of posts: #{user.posts_counter}")
     end
-    scenario 'When I click a users post, it redirects me to that posts show page.' do 
-        visit user_path
-        click_link 'Post'
-        expect(page).to have_content(post.title)
+    scenario 'When I click a users post, it redirects me to that posts show page.' do
+      visit user_path
+      click_link 'Post'
+      expect(page).to have_content(post.title)
     end
     scenario 'I can see the users first 3 posts'
-        visit user_path
-        expect(page).to have_content(user.posts.first(3))
+    visit user_path
+    expect(page).to have_content(user.posts.first(3))
   end
 end
