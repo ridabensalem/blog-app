a47a5e125cbf424f85a1f0b3c6256ace2ff8a41d
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-02 11:49:47 +0100

    Add some colums

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_132802) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_02_102300) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_132802) do
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
