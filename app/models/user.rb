a5700781b0d4c99b595b203eb30c826aff851368
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-02 11:52:52 +0100

    Add requirements for authentification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
