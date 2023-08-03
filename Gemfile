7a4804f160f65e51a5e0fc017a620cbebd83210a
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-03 20:05:19 +0100

    Applied required changes

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem 'bcrypt'
   gem 'devise'
+  gem 'rack-cors'
   gem 'web-console'
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,19 +1,60 @@
-class RegistrationsController < ApplicationController
-  def new
-    @user = User.new
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /resource
+  # def create
+  #   super
+  # end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
   end
 
-  def create
-    @user = User.new(registration_params)
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
 
-    if @user.save
-      redirect_to root_path, notice: 'Registration successful'
-    else
-      render :new
-    end
-  end
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 
-  def registration_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
-  end
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
 end
