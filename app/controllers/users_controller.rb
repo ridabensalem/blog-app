ce89ddae6a01bc48bee1f2474b170f27a249d566
Rida Bensalem <ridasbnesalmas@gmail.com>
2023-08-04 14:58:01 +0100

    Remove extra functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,4 @@ class UsersController < ApplicationController
   def show
     @user = User.includes(posts: :comments).find_by(id: params[:id])
   end
-  def list_posts
-    @user = User.find_by(id: params[:id])
-    if @user
-      @posts = @user.posts.order(created_at: :desc)
-      render json: { user: @user, posts: @posts }
-    else
-      render json: { error: "User not found" }, status: :not_found
-    end
-  end
 end
