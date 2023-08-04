07b851457992431a7f11890ecdf57fa2d692221d
Chritelle-12 <mariechristellenirere@gmail.com>
2023-08-04 13:32:57 +0200

    Add function to list all posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
   def show
     @user = User.includes(posts: :comments).find_by(id: params[:id])
   end
+  def list_posts
+    @user = User.find_by(id: params[:id])
+    if @user
+      @posts = @user.posts.order(created_at: :desc)
+      render json: { user: @user, posts: @posts }
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
+  end
 end
